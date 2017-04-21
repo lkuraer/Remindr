e2cbbb922741af1055b524c5b62bdad02572d25e
ruslan <lkuraer@gmail.com>
2017-04-21 17:02:39 +0500

    Initial Commit

---
+++ Remindr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Remindr
+//
+//  Created by Ruslan Sabirov on 4/21/17.
+//  Copyright © 2017 Ruslan Sabirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
